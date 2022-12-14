a2bc60f3d8a343e20c50bb0f173af809554daba2
Ayodeji Adesegun <93299337+ayodejiades@users.noreply.github.com>
2022-12-14 19:42:20 +0100

    changes

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,14 +15,13 @@ void times_table(void)
 	{
 		y = 0;
 		while (y < 10)
-		{  
-			z = x * y;
+		{	z = x * y;
 			if (z > 9)
 			{
 				_putchar(z / 10 + '0');
 				_putchar(z % 10 + '0');
-			}
-			else if (y != 0)
+			}	
+		 	else if (y != 0)
 			{
 				_putchar(' ');
 				_putchar(z + '0');
